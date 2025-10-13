d50be3186d90674c3cd9c650bf285a4886e192d9
threekd <threekd42@gmail.com>
2025-10-14 01:00:20 +0800

    update

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -14,38 +14,38 @@ export default defineNavbarConfig([
       { text: '3dgs-demo', link: '/medical-vis/Crystal_Presentations/3dgs_demo.md' },
     ]
   },
-  {
-    text: 'Research Field',
-    items: [
-      { text: 'Realistic Rendering', link: '/demo/README.md' },
-      { text: 'Neural Rendering', link: '/demo/README.md' },
-      { text: 'image Analysis', link: '/demo/README.md' },
-      { text: 'Generative Al', link: '/demo/README.md' },
-      { text: 'Large Scale Data', link: '/demo/README.md' },
-      { text: 'Mixed Reality', link: '/demo/README.md' },
-      { text: 'Games Engine', link: '/demo/README.md' },
-      { text: 'Data Analysis', link: '/demo/README.md' },
-    ]
-  },
-  {
-    text: 'Development Manual',
-    items: [
-      { text: 'Development Manual', link: '/demo/README.md' },
-      { text: 'Software Operation', link: '/demo/README.md' },
-      { text: 'Build Crystal Project', link: '/demo/README.md' },
-      { text: 'Code Structure', link: '/demo/README.md' },
-      { text: 'Datasets', link: '/demo/README.md' },      
-    ]
-  },
-  {
-    text: 'Publications',
-    items: [
-      { text: 'PBDVR', link: '/medical-vis/Publications/PBDVR.md' },
-      { text: 'RTR-DVR', link: '/medical-vis/Publications/RTR-DVR.md' },
-      { text: 'NNFF', link: '/medical-vis/Publications/NNFF.md' },
-      { text: 'SWKP', link: '/medical-vis/Publications/SWKP.md' },
-      { text: 'SegR3D', link: '/medical-vis/Publications/SegR3D.md' },      
-    ]
+  {  
+    text: 'Research Field',  
+    items: [  
+      { text: 'Realistic Rendering', link: '/medical-vis/research_field/Realistic_Rendering.md' },  
+      { text: 'Neural Rendering', link: '/medical-vis/research_field/Neural_Rendering.md' },  
+      { text: 'Image Analysis', link: '/medical-vis/research_field/Image_Analysis.md' },  
+      { text: 'Generative AI', link: '/medical-vis/research_field/Generative_AI.md' },  
+      { text: 'Large Scale Data', link: '/medical-vis/research_field/Large_Scale_Data.md' },  
+      { text: 'Mixed Reality', link: '/medical-vis/research_field/Mixed_Reality.md' },  
+      { text: 'Games Engine', link: '/medical-vis/research_field/Games_Engine.md' },  
+      { text: 'Data Analysis', link: '/medical-vis/research_field/Data_Analysis.md' },  
+    ]  
+  },  
+  {  
+    text: 'Development Manual',  
+    items: [  
+      { text: 'Development Manual', link: '/medical-vis/development_manual/README.md' },  
+      { text: 'Software Operation', link: '/medical-vis/development_manual/Software_Operation.md' },  
+      { text: 'Build Crystal Project', link: '/medical-vis/development_manual/Build_Crystal_Project.md' },  
+      { text: 'Code Structure', link: '/medical-vis/development_manual/Code_Structure.md' },  
+      { text: 'Datasets', link: '/medical-vis/development_manual/Datasets.md' },       
+    ]  
+  },  
+  {  
+    text: 'Publications',  
+    items: [  
+      { text: 'PBDVR', link: '/medical-vis/Publications/PBDVR.md' },  
+      { text: 'RTR-DVR', link: '/medical-vis/Publications/RTR-DVR.md' },  
+      { text: 'NNFF', link: '/medical-vis/Publications/NNFF.md' },  
+      { text: 'SWKP', link: '/medical-vis/Publications/SWKP.md' },  
+      { text: 'SegR3D', link: '/medical-vis/Publications/SegR3D.md' },       
+    ]  
   },
   {
     text: 'Team Profile',
