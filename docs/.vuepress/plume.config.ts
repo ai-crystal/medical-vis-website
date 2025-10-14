ce162684417ec723f088bd8a5f09e4ee938e60ad
threekd <threekd42@gmail.com>
2025-10-15 07:01:32 +0800

    update

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -18,7 +18,7 @@ import collections from './collections'
  * @see https://theme-plume.vuejs.press/config/basic/
  */
 export default defineThemeConfig({
-  logo: 'https://theme-plume.vuejs.press/plume.png',
+  logo: '/images/favicon-32x32.png',
 
   appearance: true,  // 配置 深色模式
 
